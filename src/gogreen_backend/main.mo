8343cb507aea95341e2bf7e140a6ac3c476ed77b
martmartinez91 <martinmartinez1991@gmail.com>
2023-12-08 13:38:07 -0500

    go

--- src/gogreen_backend/main.mo
+++ src/gogreen_backend/main.mo
@@ -1,5 +1,78 @@
+/*
 actor {
   public query func greet(name : Text) : async Text {
     return "Hello everyone my name is Martin, " # name # "!";
   };
+};
+*/
+
+import RBTree "mo:base/RBTree";
+import Nat "mo:base/Nat";
+import Text "mo:base/Text";
+import Iter "mo:base/Iter";
+
+
+actor {
+
+  var question: Text = "Available challenges to choose from";
+  var votes: RBTree.RBTree<Text, Nat> = RBTree.RBTree(Text.compare);
+  var sum = 0;
+
+  public query func getQuestion() : async Text { 
+    question 
+  };
+
+// query the list of entries and votes for each one
+// Example: 
+//      * JSON that the frontend will receive using the values above: 
+//      * [["M","0"],["P","0"],["R","0"],["T","0"]]
+
+    public query func getVotes() : async [(Text, Nat)] {
+    
+        Iter.toArray(votes.entries())
+    
+    };
+
+
+
+ // This method takes an entry to vote for, updates the data and returns the updated hashmap
+// Example input: vote("Motoko")
+// Example: 
+//      * JSON that the frontend will receive using the values above: 
+//      * [["Motoko","1"],["Python","0"],["Rust","0"],["TypeScript","0"]]
+    
+  public func vote(entry: Text) : async [(Text, Nat)] {
+
+    //Check if the entry already has votes.
+    //Note that "votes_for_entry" is of type ?Nat. This is because: 
+    // * If the entry is in the RBTree, the RBTree returns a number.
+    // * If the entry is not in the RBTree, the RBTree returns `null` for the new entry.
+    let votes_for_entry :?Nat = votes.get(entry);
+    
+    //Need to be explicit about what to do when it is null or a number so every case is taken care of
+    let current_votes_for_entry : Nat = switch votes_for_entry {
+      case null 0;
+      case (?Nat) Nat;
+    };
+
+    //once we have the number of votes, update the votes for the entry
+    votes.put(entry, current_votes_for_entry + 1);
+
+    //Return the number of votes as an array (so frontend can display it)
+    Iter.toArray(votes.entries())
+  };
+
+  public func resetVotes() : async [(Text, Nat)] {
+      votes.put("Bottle", 0);
+      votes.put("Recycle bin", 0);
+      votes.put("Electric car", 0);
+      votes.put("Vegetarian food", 0);
+      Iter.toArray(votes.entries())
+  };
+
+  public func resetInd() : async [(Text, Nat)] {
+      votes.put("Electric car", 0);
+      Iter.toArray(votes.entries())
+  };
+
 };
--- src/gogreen_frontend/src/index.js
+++ src/gogreen_frontend/src/index.js
@@ -1,3 +1,4 @@
+/*
 import { gogreen_backend } from "../../declarations/gogreen_backend";
 
 document.querySelector("form").addEventListener("submit", async (e) => {
@@ -17,3 +18,125 @@ document.querySelector("form").addEventListener("submit", async (e) => {
 
   return false;
 });
+*/
+
+const pollForm = document.getElementById("radioForm");
+const resultsDiv = document.getElementById('results');
+const resetButton = document.getElementById('reset');
+//const resetIndButton = document.getElementById('resetInd');
+
+
+//Note we will use "poll_backend" in this JavaScript code a few times to call the backend
+import { poll_backend } from "../../declarations/poll_backend";
+
+//1. LOCAL DATA
+const pollResults = {
+    "Recycle bin": 0,
+    "Bottle": 0,
+    "Electric car": 0,
+    "Vegetarian food": 0
+};
+
+var total = 0;
+
+
+//2. EVENT LISTENERS
+
+//Load the Simple Poll's question from the backend when the app loads
+document.addEventListener('DOMContentLoaded', async (e) => {
+   //note that this is at beginning of the submit callback, this is deliberate
+  //This is so the default behavior is set BEFORE the awaits are called below
+  e.preventDefault();
+ 
+  // Query the question from the backend
+  const question = await poll_backend.getQuestion();
+  document.getElementById("question").innerText = question;
+
+  //Query the vote counts for each option
+  // Example JSON that the frontend will get using the values above
+  // [["Motoko","0"],["Python","0"],["Rust","0"],["TypeScript","0"]]
+  const voteCounts = await poll_backend.getVotes();
+  updateLocalVoteCounts(voteCounts);
+  displayResults();
+  return false;
+}, false);
+
+//Event listener that listens for when the form is submitted.
+//When the form is submitted with an option, it calls the backend canister
+//via "await poll_backend.vote(selectedOption)"
+pollForm.addEventListener('submit', async (e) => {
+  //note that this is at beginning of the submit callback, this is deliberate
+  //This is so the default behavior is set BEFORE the awaits are called below
+  e.preventDefault(); 
+
+  const formData = new FormData(pollForm);
+  const checkedValue = formData.get("option");
+
+  const updatedVoteCounts = await poll_backend.vote(checkedValue);
+  console.log("Returning from await...")
+  console.log(updatedVoteCounts);
+  updateLocalVoteCounts(updatedVoteCounts);
+  displayResults();
+  return false;
+}, false);
+
+resetButton.addEventListener('click', async (e) => {
+
+    e.preventDefault();
+    
+    //Reset the options in the backend
+    await poll_backend.resetVotes();
+    const voteCounts = await poll_backend.getVotes();
+    updateLocalVoteCounts(voteCounts);
+
+    //re-render the results once the votes are reset in the backend
+    displayResults();
+    return false;
+}, false);
+
+
+
+//3. HELPER FUNCTIONS
+
+//Helper vanilla JS function to create the HTML to render the results of the poll
+function displayResults() {
+  let resultHTML = '<ul>';
+  for (let key in pollResults) {
+      resultHTML += '<li><strong>' + key + '</strong>: ' + pollResults[key] + '</li>';
+  }
+  resultHTML += '<li><strong>' + "Total" + '</strong>: ' + total + '</li>';
+  resultHTML += '</ul>';
+  resultsDiv.innerHTML = resultHTML;
+  //resultsTotal.innerHTML = string(pollTotal);
+};
+
+//This helper updates the local JS object that teh browser holds
+// Example JSON that the frontend will get using the values above
+  // [["Motoko","0"],["Python","0"],["Rust","0"],["TypeScript","0"]]
+function updateLocalVoteCounts(arrayOfVoteArrays){
+
+  var multiplier = 1;
+  total = 0;
+
+  for (let voteArray of arrayOfVoteArrays) {
+    //Example voteArray -> ["Motoko","0"]
+    let voteOption = voteArray[0];
+    let voteCount = voteArray[1];
+    
+    if (voteOption == "Recycle bin"){
+      multiplier = 20;
+    }else if (voteOption == "Bottle"){
+      multiplier = 5;
+    }else if (voteOption == "Electric car"){
+      multiplier = 10;
+    }else if (voteOption == "Vegetarian food"){
+      multiplier = 1;
+    }
+    
+    //let aux = number(voteCount)*multiplier;
+    pollResults[voteOption] = Number(voteCount)*multiplier;
+    total = total + pollResults[voteOption];
+    //pollTotal = pollTotal + voteCount;
+  }
+
+};
